f64fed692a8e41542f1be80a44db12f38dfef771
ArjitSingh09070 <singh25arjit@gmail.com>
2025-11-13 14:46:41 +0530

    Session-3 of PSPC Lab File

---
+++ case.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char name[20];
+    printf("Enter Your Name : ");
+    scanf("%s", name);
+
+    for (int i = 0; i < strlen(name); i++)
+    {
+
+        if (name[i] >= 'a' || name[i] <= 'z')
+        {
+            name[i] = name[i] - 32;
+        }
+        else
+        {
+            printf("Invalid Input!\n");
+        }
+    }
+
+    printf("UpperCase : %s\n", name);
+    return 0;
+}
---
+++ swap3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int x, int y);
+
+int main(void)
+{
+    int a = 7;
+    int b = 9;
+    int temp;
+
+    printf("Before Swapping, a = %d and b = %d\n", a , b);
+    swap(a , b);
+
+}
+
+void swap(int x, int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+
+    printf("After Swapping, a = %d and b = %d\n", x , y);
+}
