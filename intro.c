264105b324b3b6983b2092ca497cddfb801cba5f
ArjitSingh09070 <singh25arjit@gmail.com>
2026-01-07 11:37:56 +0530

    Semester-1

--- intro.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
-{
-    printf("Hello, My Name is Arjit Singh\n");
-    printf("I am an Engineering Student\n");
-    printf("Currently pursuing my Bachelor's in Technology in Computer Science and Engineering\n");
-}
