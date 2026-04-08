3e0a8f006d2e4ea0b134c14f3d5c20244ad3ca6f
Arjit <singh25arjit@gmail.com>
2026-04-08 10:52:59 +0530

    Changing p2 will change p1

--- oops/Inter/Copy/8-4-3.cpp
+++ oops/Inter/Copy/8-4-3.cpp
@@ -1,14 +1,13 @@
-//shallow copy - 2
-//changing p1 won't change p2.
+//shallow copy - 3
+//changing p2 will change p1.
 #include <iostream>
 #include <string.h>
 using namespace std;
-
 class person{
     public:
     int age;
-    char * name;
-    person(int age, char * name){
+    char *name;
+    person(int age, char *name){
         this -> age = age;
         this -> name = new char[strlen(name) + 1];
         strcpy(this -> name, name);
@@ -16,9 +15,9 @@ class person{
 };
 int main(void){
     person p1(19, "Peter");
-    person p2(p1); //shallow copy executed, and will have the same name as p1.
-    p2.age = 20;
-    p1.name = "Tony";
+    person p2(p1);
+    p2.age = 34;
+    p2.name[0] = 'T';
 
     cout << p1.name << " " << p2.name << endl;
     cout << p1.age << " " << p2.age << endl;
