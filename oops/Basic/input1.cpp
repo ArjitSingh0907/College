f6e723916ffb3d7807a00421f021d561e8cf0957
Arjit <singh25arjit@gmail.com>
2026-05-04 10:04:12 +0530

    input-2

---
+++ oops/Basic/input1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    bool marks;
+    cout << "Enter your marks: " << endl;
+    cin >> marks;
+    cout << "Marks are: " << marks << endl;
+    return 0;
+}
