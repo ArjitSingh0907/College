fe51793e652e0ae0497e31145a8db042dadd0e05
ArjitSingh0907 <singh25arjit@gmail.com>
2026-08-10 12:44:41 +0530

    starting element of a vector'

---
+++ itr1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void){
+    vector <int> v = {1, 2, 3, 4, 5};
+    cout << "Vector Begin : " << *v.begin() << endl;
+    return 0;
+}
