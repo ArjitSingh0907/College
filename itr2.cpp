3b7476da8ab00a86acbfd709fe3ebc3bedf5870b
ArjitSingh0907 <singh25arjit@gmail.com>
2026-08-10 12:52:19 +0530

    starting and ending value of a vector using itr

--- itr2.cpp
+++ itr2.cpp
@@ -2,9 +2,8 @@
 #include <vector>
 using namespace std;
 
-int main(void){
+int main(){
     vector <int> v = {1, 2, 3, 4, 5};
-    cout << "Vector Begin : " << *v.begin() << endl;
     cout << "Vector End : " << *(v.end() - 1) << endl;
     return 0;
 }
