12b39e434392d16d06e783d20407624a93490611
ArjitSingh0907 <singh25arjit@gmail.com>
2026-08-13 09:49:42 +0530

    DSC lec1 fact

--- fact.c
+++ fact.c
@@ -15,7 +15,7 @@ int main(void){
         for(i = 1; i <= n; i++){
             fact *= i;
         }
-        printf("Factorial of %d is %lld\n", n, fact);
+        printf("Factorial of %d is %d\n", n, fact);
     }
     return 0;
 }
